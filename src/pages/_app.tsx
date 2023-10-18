0e0ef8dd6c2f8ae36190731402f8ae80d5465065
Aeternus185 <andryp_@hotmail.com>
2023-10-18 13:32:55 -0400

    style: 💄 add Space Grotesk font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,23 @@ import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-space-grotesk",
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={`${spaceGrotesk.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
