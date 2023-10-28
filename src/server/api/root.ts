848f59b63a268fa1b4f2a6b23ba2c8b2e180bb85
Aeternus185 <andryp_@hotmail.com>
2023-10-27 21:49:49 -0400

    feat: ✨ add productHuntVotes router to api

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "@/server/api/routers/post";
+import { productHuntVotes } from "@/server/api/routers/productHuntVotes";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  productHuntVotes: productHuntVotes,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/productHuntVotes.ts
@@ -0,0 +1,60 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { env } from "@/env.mjs";
+import fetch from "node-fetch";
+
+interface PostResponse {
+  data: {
+    post: {
+      id: string;
+      name: string;
+      votesCount: number;
+      slug: string;
+    } | null;
+  };
+}
+
+export const productHuntVotes = createTRPCRouter({
+  getBySlug: publicProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ input }) => {
+      const query = `
+        query {
+          post(slug: "${input.slug}") {
+            id,
+            name,
+            votesCount,
+            slug
+          }
+        }
+      `;
+
+      try {
+        const response = await fetch(
+          "https://api.producthunt.com/v2/api/graphql",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${env.PRODUCT_HUNT_DEV_TOKEN}`,
+            },
+            body: JSON.stringify({ query }),
+          },
+        );
+
+        const data = (await response.json()) as PostResponse;
+
+        if (data.data.post === null) {
+          return "Post not found";
+        }
+
+        return data.data.post;
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error(error.message);
+        } else {
+          throw new Error("An unknown error occurred");
+        }
+      }
+    }),
+});
