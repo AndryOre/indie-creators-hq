67e13f186b01a42290c68fd134bd5a303ec657f9
Aeternus185 <andryp_@hotmail.com>
2023-11-03 17:51:41 -0400

    feat: ✨ add productHuntVotes api

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,4 @@
+import { productHuntVotes } from "@/server/api/routers/productHuntVotes";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -5,7 +6,9 @@ import { createTRPCRouter } from "@/server/api/trpc";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  productHuntVotes: productHuntVotes,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/productHuntVotes.ts
@@ -0,0 +1,61 @@
+import { env } from "@/env.mjs";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+import fetch from "node-fetch";
+import { z } from "zod";
+
+interface PostResponse {
+  data: {
+    post: {
+      id: string;
+      name: string;
+      votesCount: number;
+      slug: string;
+    } | null;
+  };
+}
+
+export const productHuntVotes = createTRPCRouter({
+  getBySlug: publicProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ input }) => {
+      const query = `
+        query {
+          post(slug: "${input.slug}") {
+            id,
+            name,
+            votesCount,
+            slug
+          }
+        }
+      `;
+
+      try {
+        const response = await fetch(
+          "https://api.producthunt.com/v2/api/graphql",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${env.PRODUCT_HUNT_DEV_TOKEN}`,
+            },
+            body: JSON.stringify({ query }),
+          },
+        );
+
+        const data = (await response.json()) as PostResponse;
+
+        if (data.data.post === null) {
+          return "Post not found";
+        }
+
+        return data.data.post;
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new Error(error.message);
+        } else {
+          throw new Error("An unknown error occurred");
+        }
+      }
+    }),
+});
