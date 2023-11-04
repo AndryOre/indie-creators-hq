591efd31efa4609805876fd9e5c55150e3820d44
Aeternus185 <andryp_@hotmail.com>
2023-11-03 22:59:38 -0400

    feat: ✨ add PopularProjects component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,6 @@ export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export * from "./themeProvider";
 export * from "./dotPattern";
+export * from "./spinner";
+export * from "./carousel";
+export * from "./popularProjects";
