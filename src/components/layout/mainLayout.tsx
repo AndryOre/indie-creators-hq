ea8df7b250b5d952dec8e2fc974413940754fecc
Aeternus185 <andryp_@hotmail.com>
2023-11-03 23:19:54 -0400

    feat: ✨ add Featured Side Projects section

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -14,9 +14,9 @@ export default function MainLayout({ children }: MainLayoutProps): JSX.Element {
       <Head>
         <title>Indie Creators HQ</title>
       </Head>
-      <div className="flex h-screen flex-col">
+      <div className="flex flex-col">
         <Header />
-        <main className="flex grow flex-col items-center justify-center px-4 py-14 lg:px-16 xl:px-40">
+        <main className="flex flex-col items-center overflow-x-hidden justify-center px-4 lg:px-16 xl:px-40">
           {children}
         </main>
         <Footer />
