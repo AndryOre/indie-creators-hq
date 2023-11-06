55346b680587ffa3e2542129f11875db3e40281a
Carlos Moreno <cjmd963@gmail.com>
2023-11-05 20:55:25 -0500

    feat: :sparkles: add faqs section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import {
   Button,
   DottedPattern,
+  Faqs,
   MainLayout,
   PopularProjects,
 } from "@/components";
@@ -71,6 +72,19 @@ const Home: NextPage = () => {
               {t("popularProjects.joinNow")}
             </Button>
           </Link>
+        </div>
+      </div>
+      <div className="flex justify-between flex-col lg:flex-row gap-16 py-16 ">
+        <div className="w-full lg:w-2/5 gap-4 flex flex-col">
+          <div className="text-3xl font-bold text-secondary dark:text-primary lg:text-4xl xl:text-5xl">
+            {t("faqs.title")}
+          </div>
+          <div className="text-muted-foreground text-justify">
+            {t("faqs.desc")}
+          </div>
+        </div>
+        <div className="w-full lg:w-3/5">
+          <Faqs />
         </div>
       </div>
     </MainLayout>
